a81c123a4b0847351e1f527b6288d67d7d50018a
Maicon Silveira <maiconsb2041@gmail.com>
2024-01-12 16:08:33 -0300

    Projeto inicializado

---
+++ src/main/java/com/devsuperior/ProjetoORM/ProjetoOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ProjetoORM/ProjetoOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
